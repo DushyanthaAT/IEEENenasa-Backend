5a48bed73678f9ab82b901553116bc036695ee40
DushyanthaAT <dushyantha721@gmail.com>
2025-01-03 02:11:37 +0530

    Update post.controller.js

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,27 +1,11 @@
 import Post from "../models/post.model.js";
 
-import supabase from "../config/supabaseClient.js";
 
-const uploadToSupabase = async (file) => {
-  const filePath = `events/${Date.now()}-${file.originalname}`;
-  const { data, error } = await supabase.storage
-    .from("events-bucket")
-    .upload(filePath, file.buffer, {
-      contentType: file.mimetype,
-    });
-  if (error) throw new Error(error.message);
-  return `${process.env.SUPABASE_URL}/storage/v1/object/public/events-bucket/${filePath}`;
-};
 
 export const create = async (req, res, next) => {
   try {
-    // const imageUrls = await Promise.all(
-    //   req.files.map((file) => uploadToSupabase(file))
-    // );
-
     const newPost = new Post({
       ...req.body,
-      // images: imageUrls,
     });
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
