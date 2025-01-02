db142411892d38e131e28e9e369e96d45591110e
DushyanthaAT <dushyantha721@gmail.com>
2025-01-02 23:24:15 +0530

    Update post.route.js

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { create, getPosts } from "../controllers/post.controller.js";
+import {
+  create,
+  deletePost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create-post", create);
 router.get("/getposts", getPosts);
+router.delete("/deletepost/:id", deletePost);
+router.put("/updatepost/:id", updatePost);
 
 export default router;
